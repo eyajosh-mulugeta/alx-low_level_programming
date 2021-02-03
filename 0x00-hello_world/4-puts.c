e35ee42fceec1aade7e556fe3e4217f1cdb17fa8
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-03 18:36:06 +0000

    Completed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - This main function prints a string message about programming to the console.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 
 return (0);
 }
