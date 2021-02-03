d6e96cdc1e71804c181aa62b74187b7516b1a5a2
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-03 17:55:24 +0000

    Completed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+0;276;0c#include <stdio.h>
 
 int main(void)
 {
-char str[] = "\"Programming is like building a multilingual puzzle";
+char str[] = "\"Programming is like building a multilingual puzzle\n";
 puts(str);
 return (0);
 }
