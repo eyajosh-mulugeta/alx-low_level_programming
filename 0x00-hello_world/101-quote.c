b6d07c1a06e4ea3c4200ef9015c4a3a3ead3b3eb
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-03 20:47:13 +0000

    Completed task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main function prints a string message to the console.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\"\
+-Dora Korpar, 2015-10-19\n");
+return (0);
+}
