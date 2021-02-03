64b38304a169e9837e0095f8bb32301c73534c67
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-03 18:27:33 +0000

    Completed task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/*
+ * This program prints a string message to the console.
+ */
+
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
