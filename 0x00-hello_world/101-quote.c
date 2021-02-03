d375d93855a78897e498bdf2a4580714c4c155a4
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-03 20:59:20 +0000

    Completed task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - the main function prints a string message to the console.
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" -Dora Korpar\
-, 2015-10-19\n");
-return (0);
+write(2, "and that piece of art is useful\" -Dora Korpar\
+, 2015-10-19\n", 59);
+return (1);
 }
