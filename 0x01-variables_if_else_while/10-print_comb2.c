14f506c2b6d62aadf1c5817080213e88d6e94b59
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-04 14:42:00 +0000

    Completed task 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -10,13 +10,13 @@ int main(void)
 int num1;
 int num2;
 
-for (num1 = 0; num1 <= 99; num1++)
+for (num1 = 0; num1 <= 9; num1++)
 {
-for (num2 = 0; num2 <= 99; num2++)
+for (num2 = 0; num2 <= 9; num2++)
 {
 putchar(num1 + '0');
 putchar(num2 + '0');
-if (num2 != 99)
+if (num2 != 9)
 {
 putchar(',');
 putchar(' ');
