f8642075158a102207d80644c84a958a7eb8f027
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-04 08:57:16 +0000

    Completed task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - the main function determines,
  * whether the value of n is +ve, -ve or zero.
@@ -8,17 +9,17 @@
  */
 int main(void)
 {
-  int n;
-  srand(time(0));
- 
-  n = rand() - RAND_MAX / 2;
+int n;
+srand(time(0));
 
- if(n > 0)
-   printf("%d is positive\n", n);
- else if(n < 0)
-   printf("%d is negative\n", n);
- else
-   printf("%d is zero\n", n);
+n = rand() - RAND_MAX / 2;
 
- return (0);
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+
+return (0);
 }
