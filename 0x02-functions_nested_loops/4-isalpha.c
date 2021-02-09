8ca7a4f8a9128c3757aaf711369f7389893726ca
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-09 17:15:17 +0000

    Tasks updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,6 @@
 #include "holberton.h"
 
 /**
- * main - this function invokes and pass a character to
- * the function _isalpha and print the returned result
- * to the console.
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-_putchar(_isalpha('*') + '0');
-return (0);
-}
-
-/**
  * _isalpha - this function tests whether a character is
  * alphabet or not and return an integer value to main().
  * @c: c is an integr value.
