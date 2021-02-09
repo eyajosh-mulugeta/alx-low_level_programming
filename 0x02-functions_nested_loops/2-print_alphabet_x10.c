30b68cc46b17b55c5fd865af0d65254c25a18bdd
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-09 17:12:04 +0000

    Tasks updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,10 @@ void print_alphabet_x10(void)
 {
 char ch;
 int i;
-for (i = 0, ch = 'a'; i <10 && ch <= 'z'; ch++, i++)
+for (i = 0; i < 10; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
+}
 }
