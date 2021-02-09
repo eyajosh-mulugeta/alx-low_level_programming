7dc00856a753a08c0490cfddd1d0fa88320cb6f7
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-09 20:18:36 +0000

    Tasks updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,19 +12,28 @@ int product;
 
 for (i = 0; i <= 9; i++)
 {
-_putchar('0');
 for (j = 0; j <= 9; j++)
 {
 product =  i * j;
 
+if (j == 0)
+{
+_putchar('0');
+}
+else if (product <= 9)
+{
 _putchar(',');
 _putchar(' ');
-
-if (prod <= 9)
 _putchar(' ');
+_putchar(product + '0')
+}
 else
-_putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');
+{
+_putchar(',');
+_putchar(' ');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
 }
 _putchar('\n');
 }
