d967c948481913e4493a15f4e5d67b88b7d7b23a
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-09 17:38:29 +0000

    Tasks updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,21 +13,21 @@ if (n > 0)
 _putchar('+');
 _putchar(',');
 _putchar(' ');
-_putchar('1');
+_putchar('1' + '0');
 return (1);
 
 else if (n < 0)
 _putchar('-');
 _putchar(',');
 _putchar(' ');
-_putchar('-1');
+_putchar('-1' + '0');
 return (-1);
 
 else
 _putchar('0');
 _putchar(',');
 _putchar(' ');
-_putchar('0');
+_putchar('0' + '0');
 return (0);
 
 }
