dccc5d11eb728eb9464138b34db74be3fe8c8043
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-09 20:04:00 +0000

    Tasks updated

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of the day from 00:00 to 23:59.
+ * Return: void.
+ */
+void jack_bauer(void)
+{
+int k;
+int m;
+
+for (k = 0; k <= 23; k++)
+for (m = 0; m <= 59; m++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+_putchar('\n');
+} 
+}
