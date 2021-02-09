8689b5c76f6ee2ae30927c05615da4e7bd15a531
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-09 18:33:22 +0000

    Tasks updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,16 @@ void print_to_98(int n)
 int i;
 
 if (n > 98)
+{
 for (i = n; i >= 98; i--)
 printf("%d, ", i);
-
+}
 
 if (n < 98)
+{
 for (i = n; i <= 98; i++)
 printf("%d, ", i);
-
+}
 
 _putchar('\n');
 }
