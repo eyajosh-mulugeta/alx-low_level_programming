82ebe4664d0896b3105d534b227819584fd9064e
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-09 17:58:56 +0000

    Tasks updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,6 +3,7 @@
 /**
  * print_last_digit - returns the last digit of a number
  * to the caller function.
+ * @n: n is an integer number.
  * Return: Always 0 (Success)
  */
 int print_last_digit(int n)
