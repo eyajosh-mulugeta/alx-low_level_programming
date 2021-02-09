941822044284d71573fb05ec53ec598543d351b6
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-09 17:13:48 +0000

    Tasks updated

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,6 @@
 #include "holberton.h"
 
 /**
- * main - the main function calls the _islower function and
- * prints the returned result to the console.
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-
-_putchar(_islower('C') + '0');
-return (0);
-}
-
-/**
  * _islower - this function checks whether a character
  * is lowercase or not and return an integer value to main().
  * @c: c is an integer value.
