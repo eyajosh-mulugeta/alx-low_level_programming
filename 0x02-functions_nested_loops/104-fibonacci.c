c0df155c873e8b4a4852db239261d815c0dded49
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-10 20:21:38 +0000

    Updated task-16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define N 50
 /**
  * main - prints the first 50 Fibonacci numbers
  *
@@ -8,18 +8,31 @@
 
 int main(void)
 {
-int i;
-long double num1 = 0, num2 = 1, sum;
-for (i = 0; i < 97; i++)
+unsigned long int r1 = 0, k1 = 1, r2 = 0, k2 = 2;
+unsigned long int d1, d2, d3;
+int count;
+
+printf("%lu, %lu, ", k1, k2);
+for (count = 2; count < 98; count++)
 {
-sum = num1 + num2;
-num1 = num2;
-num2 = sum;
-printf("%.0Lf", sum);
-if (i != 96)
+if (k1 + k2 > N || r2 > 0 || r1 > 0)
+{
+d1 = (k1 + k2) / N;
+d2 = (k1 + k2) % N;
+d3 = r1 + r2 + d1;
+r1 = r2, r2 = d3;
+k1 = k2, k2 = d2;
+printf("%lu%010lu", r2, k2);
+}
+else
+{
+d2 = k1 + k2;
+k1 = k2, k2 = d2;
+printf("%lu", k2);
+}
+if (count != 97)
 printf(", ");
 }
 printf("\n");
 return (0);
-
 }
