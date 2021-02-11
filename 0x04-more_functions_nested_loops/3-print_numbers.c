89a855a4af45949f330f978b6e54c44e6f68bf4a
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-11 07:28:49 +0000

    Completed task-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include "_putchar.c"
+/**
+ * print_numbers - a function that prints numbers from 0 to 9.
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+_putchar(i + '0');
+_putchar('\n');
+}
