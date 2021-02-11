9c864c676d097790bb232f21aa6a4aa80d2b0e6c
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-11 13:04:32 +0000

    Completed task-6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints a line that ends with a new line.
+ * @n: the number of times '_' will be printed.
+ * Return: void.
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
