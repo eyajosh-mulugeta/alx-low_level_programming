0c27fb9193825b28cb964d026965af5d06b9fbe4
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-11 07:19:56 +0000

    Completed task-2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - a function that finds the product of two integers.
+ * @a: a parameter which holds the value of the first integer.
+ * @b: a parameter which holds the value of the second integer.
+ * Return: returns the product of the 2 integers.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
