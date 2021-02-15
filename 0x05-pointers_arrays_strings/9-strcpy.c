f85b365b71541fac407981b0436e96f7381e1ab0
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-15 16:53:25 +0000

    Completed task-9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * strcpy - copies the string from one variable to another.
+ * @src: the string pointer variable which holds the addres of
+ * the value to be copied.
+ * @dest: the string pointer variable address where the copied string
+ * value from src will be stored.
+ * Return: char.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+return (strcpy(dest, src));
+}
