1948820c1261ba02bc3786231a79083db30904c9
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-16 15:01:20 +0000

    removed the strlen() built-in function and the string.h header file. Added a custom for loop to fing the length of a string.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-#include <string.h>
+
 /**
  * _puts - prints a string to the console.
  * @str: a pointer variable which holds the adress of the variable that
@@ -8,9 +8,11 @@
  */
 void _puts(char *str)
 {
+int len = 0;
 int i;
-int len;
-len = strlen(str);
+for (i = 0; s[i] != '\0'; i++)
+len++;
+
 for (i = 0; i < len; i++)
 _putchar(str[i]);
 _putchar('\n');
