0a3c3fc8d023b5aeb305c605171993c5e130b417
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-16 15:30:01 +0000

    removed the strlen() built-in function and the string.h header file. Added a custom for loop to find the length of a string.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,19 @@
 #include "holberton.h"
-#include <string.h>
+
 
 /**
- * -
- * Return:
+ * puts2 - A function that prints every other character of a string.
+ * @str: a char type parameter which holds the address of the string value.
+ * Return: void.
  */
 void puts2(char *str)
 {
+int len = 0;
 int i;
-int len;
 
-len = strlen(str);
+for (i = 0; str[i] != '\0'; i++)
+len++;
+
 for (i = 0; i < len; i+=2)
 _putchar(str[i]);
 _putchar('\n');
