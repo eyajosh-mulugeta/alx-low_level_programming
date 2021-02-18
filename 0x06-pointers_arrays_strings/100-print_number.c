e76e8dc2f1fdca2fe56375ebadb9e52a9715e6a1
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-18 20:58:51 +0000

    Completed task-9.

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,22 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_number - Prints integer numbers to the console using
- * a custom made _putchar function.
- * @n: holds the integer value to be printed.
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
 void print_number(int n)
 {
-unsigned int n1;
-n1 = n;
+unsigned int num = n;
+
 if (n < 0)
 {
 _putchar('-');
-n1 = -n;
+num = -num;
 }
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+
+if ((num / 10) > 0)
+print_number(num / 10);
+
+_putchar((num % 10) + '0');
 }
