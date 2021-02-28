b13409c53628b1d9189a61290d9c644a0e07eeb5
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-02-28 20:20:56 +0000

    Completed task-1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument passed.
+ * @argc: number of command line argument.
+ * @argv: array of command line argument.
+ * Return: Always 0 (Success).
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+if (argc >= 1)
+printf("%d\n", argc - 1);
+return (0);
+}
