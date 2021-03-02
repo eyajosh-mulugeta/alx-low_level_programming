c5b22e864730cc1ce42ae7d67fb137a72b52f58c
eyajosh-mulugeta <eyajosh1@gmail.com>
2021-03-02 19:41:43 +0000

    Completed task-5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: ac value.
+ * @av: av value.
+ * Return: pointer.
+ */
+char *argstostr(int ac, char **av)
+{
+char *str;
+int arg, byte, index, size = ac;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+
+for (arg = 0; arg < ac; arg++)
+{
+for (byte = 0; av[arg][byte]; byte++)
+size++;
+}
+
+str = malloc(sizeof(char) * size + 1);
+
+if (str == NULL)
+return (NULL);
+
+index = 0;
+
+for (arg = 0; arg < ac; arg++)
+{
+for (byte = 0; av[arg][byte]; byte++)
+str[index++] = av[arg][byte];
+
+str[index++] = '\n';
+}
+
+str[size] = '\0';
+
+return (str);
+}
